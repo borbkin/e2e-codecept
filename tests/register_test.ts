d48de322a36b7fb54e2575417b617e5b1fbfd1da
borbkin <borbkin@users.noreply.github.com>
2025-05-03 12:37:27 +0000

    deploy: 9f14b9ac31b520aceafe7bfd41ab2112e252f735

--- tests/register_test.ts
+++
@@ -1,9 +0,0 @@
-Feature('User Registration');
-
-Scenario('Регистрация нового пользователя', async ({ I }) => {
-  const name = 'Тестовый Котик';
-  const email = `test_${Date.now()}@example.com`;
-  const password = '123456';
-
-  await I.registerNewUser(name, email, password);
-});
