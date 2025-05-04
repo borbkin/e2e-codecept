f10b389e912bfba54193b69ca57048338bd26b73
Boris Tereshchenko <boristereshchenko@gmail.com>
2025-05-04 17:40:13 +0300

    fix again

--- src/utils/allureHelper.ts
+++ src/utils/allureHelper.ts
@@ -11,25 +11,28 @@ const noop = {
 const globalAllure = (global.allure ?? noop) as ReturnType<typeof AllureApi>;
 
 export const allureHelper = {
-  setEpic(epic: string) {
-    globalAllure.label('epic', epic);
-  },
-  setFeature(feature: string) {
-    globalAllure.label('feature', feature);
-  },
-  setStory(story: string) {
-    globalAllure.label('story', story);
-  },
-  setSeverity(level: 'blocker' | 'critical' | 'normal' | 'minor' | 'trivial') {
-    globalAllure.severity(level);
-  },
-  addParameter(name: string, value: string) {
-    globalAllure.parameter(name, value);
-  },
-  async step(name: string, body: () => Promise<void>) {
-    return globalAllure.step(name, body);
-  },
-  attach(name: string, content: string | Buffer, type: string) {
-    globalAllure.attachment(name, content, type);
-  }
-};
+    setEpic(epic: string) {
+      global.allure?.label?.('epic', epic);
+    },
+    setFeature(feature: string) {
+      global.allure?.label?.('feature', feature);
+    },
+    setStory(story: string) {
+      global.allure?.label?.('story', story);
+    },
+    setSeverity(level: 'blocker' | 'critical' | 'normal' | 'minor' | 'trivial') {
+      global.allure?.severity?.(level);
+    },
+    addParameter(name: string, value: string) {
+      global.allure?.parameter?.(name, value);
+    },
+    async step(name: string, body: () => Promise<void>) {
+      if (global.allure?.step) {
+        return global.allure.step(name, body);
+      }
+      return body(); // fallback
+    },
+    attach(name: string, content: string | Buffer, type: string) {
+      global.allure?.attachment?.(name, content, type);
+    }
+  };
